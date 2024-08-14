e3c1a46bbd66fdaf4b54fad57faa90829f2bce90
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-08-14 11:38:55 +0500

    Create module_7_2.py

---
+++ module_7_2.py
@@ -0,0 +1,29 @@
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    file = open(file_name, 'w', encoding='utf-8')
+    for line_num, string in enumerate(strings, start=1):
+        byte_position = file.tell()
+        file.write(string + '\n')
+        strings_positions[(line_num, byte_position)] = string
+    file.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
